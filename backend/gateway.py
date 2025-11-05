6782b02ce32f5ff2053c6996e64c66df1c0949b4
Henock Tesfaye Diressie <jupi@Henocks-MacBook-Air.local>
2025-11-05 20:31:25 +0300

    Add CSV processing to aggregate sales by department in upload endpoint

--- backend/gateway.py
+++ backend/gateway.py
@@ -1,24 +1,47 @@
-from fastapi import FastAPI, UploadFile, FileResponse
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import FileResponse
 import uuid
 import os
+import csv
+from collections import defaultdict
+import io
+import uvicorn
 
 app = FastAPI()
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+
 @app.post("/upload")
 async def upload_file(file: UploadFile):
-    file_id = str(uuid.uuid4())
-    file_path = os.path.join(UPLOAD_DIR, f"{file_id}.csv")
-    with open(file_path, "wb") as f:
-        f.write(await file.read())
-    return {"file_id": file_id}
+    sales = defaultdict(int)
+    file.file.seek(0)
+    reader = csv.reader(io.TextIOWrapper(file.file, encoding="utf-8"))
+    _ = next(reader)  # Skip header
+    for row in reader:
+        dept, _, num_sales = row
+        sales[dept] += int(num_sales)
+
+    result_id = str(uuid.uuid4())
+    result_path = os.path.join(UPLOAD_DIR, f"{result_id}.csv")
+    with open(result_path, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Department Name", "Total Sales"])
+        for dept, total in sales.items():
+            writer.writerow([dept, total])
+
+    return {"result_file_id": result_id}
+
 
 @app.get("/download/{file_id}")
 async def download_file(file_id: str):
     file_path = os.path.join(UPLOAD_DIR, f"{file_id}.csv")
     if os.path.exists(file_path):
-        return FileResponse(file_path, media_type='text/csv', filename=f"{file_id}.csv")
+        return FileResponse(file_path, media_type="text/csv", filename=f"{file_id}.csv",)
     else:
         return {"error": "File not found"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
+import uvicorn
+from gateway import app
+
 def main():
     print("Hello from mereb-gprc!")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
